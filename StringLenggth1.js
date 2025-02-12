63c1a2fd28312cca6f1c6075a2d9f50841b18968
Anastasia Sirakova <adsirakova.93@gmail.com>
2025-02-12 06:38:10 +0200

    .

--- StringLenggth1.js
+++
@@ -1,9 +0,0 @@
-function solve (first, second, third){
-    let sumLength=first.length + second.length + third.length;
-    let averageLength = Math.floor (sumLength/3);
-    console.log(sumLength);
-    console.log(averageLength);
-}
-
-solve('chocolate', 'ice cream', 'cake')
-solve ('pasta', '5', '22.3')
--- daysInMonth.js
+++
@@ -1,8 +0,0 @@
-function days (month, year){
-    let date = new Date(year, month, 0);
-    console.log(date.getDate());
-    
-}
-
-days(1, 2012)
-days (2, 2021)
